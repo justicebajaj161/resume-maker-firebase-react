d54532c971f19a5e32fc4439e61a98f015e79eaf
Aditya <adityabajaj161@gmail.com>
2023-08-26 13:55:51 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import  Navbar  from './components/navbar';
+import Signin from './signin/signin';
+import Signup from './signup/signup';
+import Details from './components/details';
+import Resume from './components/resume';
+import ProtectedRoutes from './protectedroutes/protectedroutes';
+import { BallTriangle } from "react-loader-spinner";
+import { useContext } from 'react';
+import { LoadingContext } from './contexts/loadingContext';
 function App() {
+  const {loading, setLoading} = useContext(LoadingContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    <BrowserRouter>
+    {loading ? (
+              <div className="loading-overlay">
+                <BallTriangle
+                  color="maroon"
+                  height={100}
+                  width={100}
+                />
+              </div>
+            ) : (
+              <ToastContainer />
+            )}
+    
+      
+         
+          <Routes>
+
+           
+            
+            
+            <Route path="/" element={<Navbar />}>
+            <Route index element={<ProtectedRoutes><Details /></ProtectedRoutes>} />
+            <Route path="/resume" element={<ProtectedRoutes><Resume /></ProtectedRoutes>} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+             
+            </Route>
+         
+
+          </Routes>
+    
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { LoadingProvider } from './contexts/loadingContext';
+import { AuthProvider } from './contexts/authContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthProvider>
+    <LoadingProvider>
     <App />
+    </LoadingProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
